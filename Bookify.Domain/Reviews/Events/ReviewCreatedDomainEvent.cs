815334af972e127ba10f8e5fe1659c05ef0fb1e3
William Harrison <william.harrison@xpeditegoc.com>
2026-01-14 16:05:15 +0000

    update domain error with final required entities

---
+++ Bookify.Domain/Reviews/Events/ReviewCreatedDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using Bookify.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookify.Domain.Reviews.Events
+{
+    public sealed record ReviewCreatedDomainEvent(Guid ReviewId) : IDomainEvent;
+}
