b1b9c630d436d85b8ca27c6aa01297e1b650851e
William Harrison <william.harrison@xpeditegoc.com>
2026-01-14 16:37:03 +0000

    configure dependency injection

---
+++ Bookify.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Bookify.Domain.Bookings;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookify.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            });
+
+            services.AddTransient<PricingService>();
+
+            return services;
+        }
+    }
+}
