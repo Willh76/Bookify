9ed213a16e68b587eb2aed873f1fe8f208f3946f
William Harrison <william.harrison@xpeditegoc.com>
2026-01-21 07:15:13 +0000

    update apertments query

--- Bookify.Application/Apartments/SearchApartments/SearchApartmentQueryHandler.cs
+++ Bookify.Application/Apartments/SearchApartments/SearchApartmentQueryHandler.cs
@@ -34,8 +34,29 @@ namespace Bookify.Application.Apartments.SearchApartments
             using IDbConnection connection = _sqlConnectionFactory.CreateConnection();
 
             const string sql = """
-                SELECT * FROM Apartment
-                """;
+            SELECT
+                a.id AS Id,
+                a.name AS Name,
+                a.description AS Description,
+                a.price_amount AS Price,
+                a.price_currency AS Currency,
+                a.address_country AS Country,
+                a.address_state AS State,
+                a.address_zip_code AS ZipCode,
+                a.address_city AS City,
+                a.address_street AS Street
+            FROM apartments AS a
+            WHERE NOT EXISTS
+            (
+                SELECT 1
+                FROM bookings AS b
+                WHERE
+                    b.apartment_id = a.id AND
+                    b.duration_start <= @EndDate AND
+                    b.duration_end >= @StartDate AND
+                    b.status = ANY(@ActiveBookingStatuses)
+            )
+            """;
 
             var apartments = await connection
                 .QueryAsync<ApartmentResponse, AddressResponse, ApartmentResponse>(
