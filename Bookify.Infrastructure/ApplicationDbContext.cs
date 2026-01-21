20576f3059423b30843e0b83ad55e8b775a4f231
William Harrison <william.harrison@xpeditegoc.com>
2026-01-21 16:45:12 +0000

    add domain event publishing

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -1,15 +1,18 @@
 ﻿using Bookify.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Infrastructure
 {
     public sealed class ApplicationDbContext : DbContext, IUnitOfWork
     {
+        private readonly IPublisher _publisher;
 
-        public ApplicationDbContext(DbContextOptions options)
-            :base(options)
+        public ApplicationDbContext(DbContextOptions options,
+            IPublisher publisher)
+            : base(options)
         {
-            
+            _publisher = publisher;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -17,6 +20,36 @@ namespace Bookify.Infrastructure
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            int result = await base.SaveChangesAsync(cancellationToken);
+
+            await PublishDomainEventsAsync();
+
+            return result;
+        }
+
+        private async Task PublishDomainEventsAsync()
+        {
+            List<IDomainEvent> domainEvents = ChangeTracker
+                .Entries<Entity>()
+                .Select(e => e.Entity)
+                .SelectMany(entity =>
+                {
+                    IReadOnlyList<IDomainEvent> domainEvents = entity.GetDomainEvents();
+
+                    entity.ClearDomainEvents();
+
+                    return domainEvents;
+                })
+                .ToList();
+
+            foreach (var domainEvent in domainEvents)
+            {
+                await _publisher.Publish(domainEvent);
+            }
         }
     }
 }
