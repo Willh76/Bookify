3e08565727e0104c847bee6601115f8e2f47eec3
William Harrison <william.harrison@xpeditegoc.com>
2026-01-21 07:50:10 +0000

    register dependency injection into the infrastructure layer

---
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Email;
+using Bookify.Infrastructure.Clock;
+using Bookify.Infrastructure.Email;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookify.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
---
+++ Bookify.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,15 @@
+﻿using Bookify.Application.Abstractions.Email;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookify.Infrastructure.Email
+{
+    internal sealed class EmailService : IEmailService
+    {
+        public Task SendAsync(Domain.Users.Email recipient, string subject, string body)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
