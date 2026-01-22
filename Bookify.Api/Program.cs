061dca1fea232e94fd2ab34006d98fb7932434d8
William Harrison <william.harrison@xpeditegoc.com>
2026-01-22 09:25:40 +0000

    configure services

--- Bookify.Api/Program.cs
+++ Bookify.Api/Program.cs
@@ -1,3 +1,6 @@
+using Bookify.Application;
+using Bookify.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,9 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
