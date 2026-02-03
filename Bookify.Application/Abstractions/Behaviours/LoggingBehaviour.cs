647905ddbedc0d10e6602966740ae4d7fc945069
William Harrison <william.harrison@xpeditegoc.com>
2026-02-03 10:54:57 +0000

    add structured logging

--- Bookify.Application/Abstractions/Behaviours/LoggingBehaviour.cs
+++ Bookify.Application/Abstractions/Behaviours/LoggingBehaviour.cs
@@ -1,12 +1,15 @@
 ﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Serilog.Context;
 
 namespace Bookify.Application.Abstractions.Behaviours
 {
     public class LoggingBehaviour<TRequest, TResponse>
         : IPipelineBehavior<TRequest, TResponse>
-        where TRequest : IBaseCommand
+        where TRequest : IBaseRequest
+        where TResponse : Result
     {
         private readonly ILogger<TRequest> _logger;
 
@@ -24,18 +27,24 @@ namespace Bookify.Application.Abstractions.Behaviours
 
             try
             {
-                _logger.LogInformation("Executing command {Command}", name);
+                _logger.LogInformation("Executing request {Request}", name);
 
                 var result = await next();
 
-                _logger.LogInformation("Command {Command} processed successfully", name);
+                if (result.IsSuccess)
+                    _logger.LogInformation("Request {Request} processed successfully", name);
+                else
+                    using (LogContext.PushProperty("Error", result.Error, true))
+                    {
+                        _logger.LogError("Request {Request} processed with error", name);
+                    }
 
                 return result;
 
             }
             catch (Exception exception)
             {
-                _logger.LogError(exception, "Command {Command} processing failed", name);
+                _logger.LogError(exception, "Request {Request} processing failed", name);
 
                 throw;
             }
