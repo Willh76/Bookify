4c96e769942a907ff0962b04ac4849fec7669ecd
William Harrison <william.harrison@xpeditegoc.com>
2026-02-05 11:31:11 +0000

    update bookingscontroller to be a minimal API

--- Bookify.Api/Program.cs
+++ Bookify.Api/Program.cs
@@ -1,4 +1,7 @@
+using Asp.Versioning;
 using Asp.Versioning.ApiExplorer;
+using Asp.Versioning.Builder;
+using Bookify.Api.Controllers.Bookings;
 using Bookify.Api.Extensions;
 using Bookify.Api.OpenApi;
 using Bookify.Application;
@@ -58,6 +61,17 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+ApiVersionSet apiVersionSet = app.NewApiVersionSet()
+            .HasApiVersion(new ApiVersion(1))
+            .ReportApiVersions()
+            .Build();
+
+var routeGroupBuilder = app.MapGroup("api/v{version:apiVersion}").WithApiVersionSet(apiVersionSet);
+
+routeGroupBuilder.MapBookingEndpoints();
+
+app.MapBookingEndpoints();
 
 app.MapHealthChecks("health", new HealthCheckOptions
 {
